b135c75c9e7903fd6e497353b3896d0e534a6c9c
nnji <mailchrprog@gmail.com>
2018-03-25 21:24:27 +0200

    Basic CMake setup

---
+++ src/main.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+// use c++17
+auto main() -> int {
+	std::cout << "Hello World\n";
+}
---
+++ tests/main.cxx
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("First test") {
+	REQUIRE(3*3 == 9);
+}
